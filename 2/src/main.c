e0088094122b78f858e362bdf94a39fc40fe0677
jperezu <jmp51.21@gmail.com>
2019-02-06 13:18:45 +0100

    Ejercicio 2

--- 2/src/main.c
+++ 2/src/main.c
@@ -52,9 +52,9 @@ void task_blink(void* ignore)
 
     while(true) {
     	GPIO_OUTPUT_SET(2, 0);
-        vTaskDelayUntil(&xLastWakeTime, 1000/portTICK_RATE_MS);
+        vTaskDelayUntil(&xLastWakeTime, 500/portTICK_RATE_MS);
     	GPIO_OUTPUT_SET(2, 1);
-        vTaskDelayUntil(&xLastWakeTime, 1000/portTICK_RATE_MS);
+        vTaskDelayUntil(&xLastWakeTime, 500/portTICK_RATE_MS);
     }
 
     vTaskDelete(NULL);
